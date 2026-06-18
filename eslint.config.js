c8d27266403277434b544e5a1687425a341c3b30
joaquinacuna <joaquinacunar@gmail.com>
2026-06-18 14:58:05 -0300

    install pre commit

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ const eslint = require("@eslint/js");
 const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = defineConfig([
   {
@@ -38,7 +39,7 @@ module.exports = defineConfig([
     extends: [
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
-      "prettier",
+      eslintConfigPrettier,
     ],
     rules: {},
   }
